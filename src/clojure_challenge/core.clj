3690c78965fec65e30b587227ad160582338b69e
Edgar Mejia <edgarinhm@hotmail.com>
2023-10-29 23:07:08 -0500

    add get all invoices base function add is valid iva validation function add invoice items loop logic

--- src/clojure_challenge/core.clj
+++ src/clojure_challenge/core.clj
@@ -1,10 +1,11 @@
 (ns clojure-challenge.core
  (:require [clojure-challenge.problem-1.problem-1 :as p1]))
 
+(def invoice (clojure.edn/read-string (slurp "resources/invoice.edn")))
 
 (defn -main
  []
- (let [p1 (p1/get-all-invoice-items-by-given-conditions)]
-   (println "Starting clojure challenge p1")))
+ (let [p1 (p1/get-all-invoice-items-by-given-conditions invoice)]
+   (println "Starting clojure challenge p1" p1)))
 
 
--- src/clojure_challenge/problem_1/problem_1.clj
+++ src/clojure_challenge/problem_1/problem_1.clj
@@ -1,7 +1,16 @@
 (ns clojure-challenge.problem-1.problem-1)
-(def invoice (clojure.edn/read-string (slurp "resources/invoice.edn")))
+
+(defn isValidIVA?
+  [tax]
+  (= (:tax/rate? 19)
+      tax))
 
 (defn get-all-invoice-items-by-given-conditions
-  []
-  (println "return invoice items by given conditions" invoice))
+  "receives an invoice as an argument and returns all items that satisfy the conditions IVA 19%"
+  [{id :invoice/id items :invoice/items}]
+  (into {}
+        (->> items
+             (println))))
+
+
 
